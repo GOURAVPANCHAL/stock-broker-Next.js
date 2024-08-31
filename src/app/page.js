de82b0347de1f66cd8d1fc51dff6390bfaf90489
Gourav <gourav.panchal@hoogaaa.com>
2024-08-31 12:52:52 +0530

    m

--- src/app/page.js
+++ src/app/page.js
@@ -4,24 +4,25 @@ import HomeSlider from "./HomeSlider/page";
 import Footer from "./Footer/page";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "./globals.css"
-import Testimonials from "./Testimonials/page"
-import Faq from "./Faq/page"
-import Services from "./services/page"
-import SIP from './sip/page'
-import SIPGraph from './sipgraph/page'
-import TabItem from './tabitem/page'
-import TradeOnOur from './tradeonour/page'
+import "./globals.css";
+import Testimonials from "./Testimonials/page";
+import Faq from "./Faq/page";
+import Services from "./services/page"; // Ensure this path is correct
+import SIP from "./sip/page";
+import SIPGraph from "./sipgraph/page";
+import TabItem from "./tabitem/page";
+import TradeOnOur from "./tradeonour/page";
+
 function page() {
   return (
     <>
       <Header />
       <HomeSlider />
-      <SIPGraph/>
-      <TradeOnOur/>
+      <SIPGraph />
+      <TradeOnOur />
       <SIP />
       <Services />
-      <TabItem/>
+      <TabItem />
       <Testimonials />
       <Faq />
       <Footer />
